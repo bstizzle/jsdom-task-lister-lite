11752151a267443cd38ce81c12d77601c64dce6e
bstizzle <whereyouben@gmail.com>
2020-12-11 14:41:55 -0500

    progress

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
 });
+
+let newTask = event => {
+  event.preventDefault();
+  let task = document.createElement("li");
+  task.innerText = document.getElementById("new-task-description").value;
+}
